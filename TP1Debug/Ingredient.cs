e9de43ccb8be59c479f058ee04fa4c0a4d04b91b
Charlotte DER-BAGHDASSARIAN <derbaghc@iut-acy.local>
2020-10-12 11:52:11 +0200

    init

---
+++ TP1Debug/Ingredient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Debug
+{
+    class Ingredient
+    {
+        private int id;
+        private String libelle;
+        private int quantite;
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if(value < 1)
+                    throw new ArgumentException("L'id ne peut pas être inférieur à 1");
+                this.id = value;
+            }
+        }
+        public String Libelle
+        {
+            get
+            {
+                return this.libelle;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Le nom d'un ingrédient ne peut pas être nul.");
+                if (value.Length == 0)
+                    throw new ArgumentException("Le nom d'un ingrédient ne peut pas être vide.");
+                this.libelle = value;
+            }
+        }
+
+        public int Quantite
+        {
+            get
+            {
+                return this.quantite; 
+            }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("La quantité ne peut pas être inférieur à 1");
+                this.quantite = value;
+            }
+        }
+    }
+}
---
+++ TP1Debug/Pays.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Debug
+{
+    public class Pays
+    {
+        private int id;
+        private String nom;
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("L'id ne peut pas être inférieur à 1");
+                this.id = value;
+            }
+        }
+        
+        public String Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Le nom d'un pays ne peut pas être nul.");
+                if (value.Length == 0)
+                    throw new ArgumentException("Le nom d'un pays ne peut pas être vide.");
+                this.nom = value;
+            }
+        }
+
+        public Pays(int id, String nom)
+        {
+            this.Id = id;
+            this.Nom = nom;
+        }
+
+        public override string ToString()
+        {
+            return "Nom : " + this.nom + "\n Id : " + this.id;
+        }
+    }
+}
